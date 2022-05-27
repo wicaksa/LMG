1c3a8912191637fef88a929f718661ba4fc5b48c
Wicaksa <wicaksa_r@yahoo.com>
2022-05-27 16:58:48 +0000

    First commit

---
+++ LMG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LMG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
