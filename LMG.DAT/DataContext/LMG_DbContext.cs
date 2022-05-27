260937f6c2304709f22473ae0cde558b31fe3c5e
Wicaksa <wicaksa_r@yahoo.com>
2022-05-27 22:56:16 +0000

    Added Author, Book, BookAuthor, and Member codes

--- LMG.DAT/DataContext/LMG_DbContext.cs
+++ LMG.DAT/DataContext/LMG_DbContext.cs
@@ -1,4 +1,8 @@
-﻿using LMG.DAT.Models.Borrow;
+﻿using LMG.DAT.Models.Author;
+using LMG.DAT.Models.BookAuthor;
+using LMG.DAT.Models.Book;
+using LMG.DAT.Models.Borrow;
+using LMG.DAT.Models.Member;
 using LMG.DAT.Models.Review;
 using LMG.DAT.Models.Series;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +20,14 @@ namespace LMG.DAT.DataContext
         public DbSet<DC_Review> Review { get; set; }
         public DbSet<DC_Series> Series { get; set; }
 
+        // WM
+        public DbSet<DC_Author> Author { get; set; }
+        public DbSet<DC_Book> Book { get; set; }
+        public DbSet<DC_Member> Member { get; set; }
+
+
+
+        // Not sure what to do for this method so we left it empty. -WM
         public LMG_DbContext()
         {
         }
@@ -25,9 +37,15 @@ namespace LMG.DAT.DataContext
             modelBuilder.ApplyConfiguration(new BorrowConfiguration());
             modelBuilder.ApplyConfiguration(new ReviewConfiguration());
             modelBuilder.ApplyConfiguration(new SeriesConfiguration());
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());   
+            modelBuilder.ApplyConfiguration(new MemberConfiguration());
+            modelBuilder.ApplyConfiguration(new BookAuthorConfiguration());
+
             base.OnModelCreating(modelBuilder);
         }
 
+        // Move this later to appsettings.json - WM
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlServer("Data Source=codeday-labs-2;Integrated Security=True");
     }
