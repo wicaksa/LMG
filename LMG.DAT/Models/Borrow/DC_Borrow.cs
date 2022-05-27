6d007df6bb74808a7885b746a903e16984c94a0d
EPeralta18 <edgarpm1820@gmail.com>
2022-05-27 20:00:09 +0000

    Created a Models for Borrow, Review, and Series. Also created base data context file and data context file

---
+++ LMG.DAT/Models/Borrow/DC_Borrow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMG.DAT.Models.Borrow
+{
+    public class DC_Borrow : DataContextBase
+    {
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+        public string BorrowDate { get; set; }
+    }
+
+    public class BorrowConfiguration : IEntityTypeConfiguration<DC_Borrow>
+    {
+        public void Configure(EntityTypeBuilder<DC_Borrow> builder)
+        {
+            builder.ToTable("Borrow", "Borrow")
+                .HasKey(m => m.Id);
+            builder.Property(m => m.Id).ValueGeneratedOnAdd();
+            builder.Property(m => m.BorrowDate).HasMaxLength(16);
+
+        }
+    }
+}
