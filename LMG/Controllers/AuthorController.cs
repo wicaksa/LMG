62e0c20879dbf12394b36a0d9f98b8b0e4a13a30
EPeralta18 <edgarpm1820@gmail.com>
2022-06-07 02:18:42 +0000

    Added Controllers for BookAuthor and Author

---
+++ LMG/Controllers/AuthorController.cs
@@ -0,0 +1,97 @@
+﻿using LMG.DAT.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMG.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IGeneralUnitOfWork _uow;
+        public AuthorController(IGeneralUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        // GET: AuthorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AuthorController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            try
+            {
+                return Ok(await _uow.GetAuthorById(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: AuthorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ LMG/Controllers/BookAuthorController.cs
@@ -0,0 +1,97 @@
+﻿using LMG.DAT.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMG.Controllers
+{
+    public class BookAuthorController : Controller
+    {
+        private readonly IGeneralUnitOfWork _uow;
+        public BookAuthorController(IGeneralUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        // GET: BookAuthorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BookAuthorController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            try
+            {
+                return Ok(await _uow.GetBookAuthorById(id));
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: BookAuthorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookAuthorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookAuthorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookAuthorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookAuthorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookAuthorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
