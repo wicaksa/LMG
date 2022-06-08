c0852ad61010309e4560b8f1691cdd7470ffe5c9
EPeralta18 <edgarpm1820@gmail.com>
2022-06-08 03:38:37 +0000

    Got Api Working

---
+++ LMG.API/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using LMG.DAT.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMG.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IGeneralUnitOfWork _uow;
+
+        public BookController(IGeneralUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _uow.BookRepository.GetByIdAsync(id));
+        }
+    }
+}
